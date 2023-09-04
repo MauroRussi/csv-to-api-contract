b88251fca57c05088fd9079e64f54e1dd5be92bf
MauroRussi <mauricio.russi@fair.com>
2023-09-04 16:49:23 +0200

    Project structure

---
+++ src/main/java/co/com/mrsoft/utils/api/CSVToAPIContract.java
@@ -0,0 +1,14 @@
+package co.com.mrsoft.utils.api;
+
+import org.json.*;
+
+public class CSVToAPIContract {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Project in construction...");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
